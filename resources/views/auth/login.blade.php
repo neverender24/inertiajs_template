4b3edb9bb71c95ead489e86d19c40a394b146583
George <neverender24@gmail.com>
2022-04-07 14:34:51 +0800

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,6 @@
     <div id="loader">
         <div class="spinner"></div>
     </div>
-{{$errors}}
     <div class="peers ai-s fxw-nw h-100vh">
         <div class="d-n@sm- peer peer-greed h-100 pos-r bgr-n bgpX-c bgpY-c bgsz-cv"
             style='background-image:url("images/bg.jpg")'>
@@ -35,12 +34,25 @@
                     <h3>YOUR INFORMATION SYSTEM NAME</h3>
                 </div>
             </div>
-            <h4 class="fw-300 c-grey-900 mB-40">Login</h4>
+            <h4 class="fw-300 c-grey-900 mB-10">Login</h4>
+            @if (count($errors) > 0)
+            <div class="alert alert-danger" style="margin-bottom: unset; padding: .3rem;">
+                @foreach($errors->all() as $message)
+                <small class="text-danger"><strong>
+                        <p style="margin-bottom: unset;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor"
+                                class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16"
+                                role="img" aria-label="Warning:">
+                                <path
+                                    d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                            </svg>
+                            {{ $message }}</p>
+                    </strong></small>
+                @endforeach
+            </div>
+            @endif
             <form method="POST" action="{{ route('login') }}">
                 @csrf
-                 @if ($errors)
-                 <small class="text-danger"><strong>{{ $errors->first('email') }}</strong></small>
-                 @endif
                 <div class="mb-3">
                     <label class="text-normal text-dark form-label">Username</label>
                     <input type="email" name="email" class="form-control">
