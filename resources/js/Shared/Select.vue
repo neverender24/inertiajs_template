b9b90f0d1e98716f17b55a5d05ac3473562f0db5
javarex <raymartnayreitanong11@gmail.com>
2022-04-13 11:06:25 +0800

    vue-select v1.3

--- resources/js/Shared/Select.vue
+++ resources/js/Shared/Select.vue
@@ -1,7 +1,5 @@
 <template>
     <v-select 
-        
-        :options="collection" 
         :reduce="collection => collection.value"
         :clearable="false" 
         
@@ -9,30 +7,36 @@
 </template>
 
 <script>
-import { watch } from '@vue/runtime-core'
 
 export default {
     props: {
-        collection: Array,
 
+        form:Object,
         /**
          * List of dynamic props to watch that will trigger
          * the api call (axios response)
          */
 
-        barangays:{
-            type: String,
-            default: ''
-        }
+        // barangas:{
+        //     type: String,
+        //     default: ''
+        // },
+
+        // purok:{
+        //     type: String,
+        //     default: ''
+        // },
 
     },
 
     watch:{
-       barangays: function(value) {
-        axios.post('/barangays', {municipal_id:value}).then((response) => {
-            this.$emit('storeBarangay',response.data)
-        })
+       form: {
+           handler(){
+               this.$emit('callMethod');
+           },
+           deep:true
        }
+        
     }
 }
 </script>
